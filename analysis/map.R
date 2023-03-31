68b8750b2722baecf9dfec298a31b5ec8c45cb90
Frederic Blum <frederic_blum@eva.mpg.de>
2023-03-31 16:05:57 +0200

    update map

--- analysis/map.R
+++ analysis/map.R
@@ -16,25 +16,20 @@ spdf_sa <- ne_countries(continent = c("south america"),
 map_lex <- ggplot(data=langs) +
   geom_sf(data = spdf_sa) +
   coord_sf(ylim=c(-22.5, 1), xlim= c(-82, -55)) +
-  # coord_sf(ylim=c(-55, 10), xlim= c(-88.5, -35)) +
-  geom_point(aes(x=Longitude,y=Latitude, shape=Family, color=Family),
-             size=5) +
-  # geom_label_repel(box.padding=1,
-  #                  point.padding = 0.3,
-  #                  aes(Longitude, Latitude, label=Name),
-  #                  size=4,
-  #                  max.overlaps = Inf) +
-  # scale_shape_manual(values =c("Mayoruna"=21, "Panoan"=22, "Tacanan"=24)) +
-  scale_fill_viridis_d(guide = "legend") +
-  # labs(caption = "Data: Glottolog") +
+  geom_point(aes(x=Longitude,y=Latitude, shape=SubGroup, fill=SubGroup), size=6) +
+  geom_label_repel(box.padding=0.8,
+                   point.padding=0.8,
+                   aes(Longitude, Latitude, label=Name),
+                   size=4,
+                   max.overlaps = Inf) +
+  scale_shape_manual(values =c("Pano"=21, "Tacana"=22, "Mosetén"=23, "Movima"=24, "Uru-Chipaya"=25)) +
+  scale_fill_viridis_d(guide="legend", option="A") +
+  labs(caption = "Data: Glottolog") +
   theme_bw() +
   theme(legend.position="bottom",
         axis.title = element_text(size = rel(1.3)),
         axis.text = element_text(size = rel(1.3)),
         legend.text = element_text(size = rel(1.3))) # +
-  # guides(fill = guide_legend(
-  #   override.aes=list(shape = c(21, 22, 24)),
-  #   ""),
-  #   shape = "none")
+
 
 ggsave('images/fig_map.png', map_lex, width=2400, height=3000, units="px")
