c9d75f1c1802dfdb430ac267ed8e0393d6293059
Frederic Blum <frederic_blum@eva.mpg.de>
2023-09-30 22:54:40 +0200

    clean archives

--- raw/scripts/find_duplicates.R
+++
@@ -1,9 +0,0 @@
-library(readr)
-library(dplyr)
-library(tidyr)
-
-
-data <- read_tsv('datasets/blumpanotacana/raw/raw.tsv')
-
-data %>% group_by(DOCULECT, CONCEPT, TOKENS) %>% count() %>% filter(n>1) %>% 
-  arrange(DOCULECT, CONCEPT)
