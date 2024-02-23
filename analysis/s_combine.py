e14caf39cbabc1b2646efe4549bec8e9a0e2271c
Frederic Blum <frederic_blum@eva.mpg.de>
2024-02-23 13:09:41 +0100

    add script

---
+++ analysis/s_combine.py
@@ -0,0 +1,35 @@
+"""This script imports the Grmabank data for languages that are
+part of the blumpanotacana database.
+"""
+import sqlite3
+import tqdm
+
+
+ATTACH_GB = """attach 'grambank.sqlite3' AS gb;"""
+COMBINE = """
+SELECT *
+FROM LanguageTable AS a
+INNER JOIN gb.ValueTable AS b
+ON a.cldf_glottocode = b.cldf_languageReference;
+"""
+
+def get_db(path):
+    """Loads sqlite3 database."""
+    con = sqlite3.connect(path)
+    return con.cursor()
+
+
+output_data = [[
+    'Doculect',
+    'Concept',
+    'Form'
+]]
+
+db = get_db('../blumpanotacana.sqlite3')
+db.execute(ATTACH_GB)
+db.execute(COMBINE)
+
+gb_data = []
+for row in tqdm.tqdm(db.fetchall()):
+    print(row)
+    output_data.append(row)
